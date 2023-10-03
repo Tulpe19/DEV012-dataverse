cba425893dfd242923b92be0c71011be9173d529
Milton Di Simone <miltondisimone@gmail.com>
2023-10-03 19:32:05 -0300

    ordenamos los filtros y ajustamos el contenedor main con un único elemento h2 dentro. Se creó la función que pinta la información de las tarjetas.

--- src/main.js
+++ src/main.js
@@ -3,4 +3,8 @@ import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
 
+const root = document.getElementById("root");
+
+root.appendChild(renderItems(data));
+
 console.log(example, renderItems(data), data);
--- src/view.js
+++ src/view.js
@@ -1,6 +1,24 @@
 export const renderItems = (data) => {
   console.log(data)
   // Aquí comienza tu código y puedes retornar lo que tu necesites
-  return 'example';
+
+  const ul = document.createElement("ul");
+
+  for(const item of data) {
+    const li = document.createElement("li");
+    li.innerHTML += `
+    <dl itemscope itemtype="">
+      <dt>Nombre:</dt><dd itemprop="name">${item.name}</dd>
+      <dt>Descripción corta:</dt><dd itemprop="shortDescription">${item.shortDescription}</dd>
+      <dt>Hechos:</dt><dd itemprop="facts">${item.facts}</dd>
+    </dl>
+    `;
+
+    ul.appendChild(li);
+
+  }
+
+  return ul;
+  
 };
 
