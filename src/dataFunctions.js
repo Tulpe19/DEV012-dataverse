6a09f7d69103b2fb037d7801b9a48b44a356bb75
Milton Di Simone <miltondisimone@gmail.com>
2023-10-06 13:56:38 -0300

    se corrigió la función de filtrar y se movieron variables y addEventlistener al archivo main.js

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,38 +1,15 @@
 // Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
-import data from "./data/dataset.js";
-import { renderItems } from "./view.js";
+export const filterData = (data, filterBy, value) => {
+  const filteredData = data.filter((item) => item[filterBy] === value);
 
-const nombresSelect = document.getElementById("nombres-select");
-
-const personajes = data;
-
-const root = document.getElementById("root");
-
-nombresSelect.addEventListener("input", (event) => {
-  console.log("funciona", event.target.value);
-
-  const selectedName = event.target.value;
-
-  const foundPersonaje = personajes.find(
-    (personaje) => personaje.name === selectedName
-  );
-
-  root.innerHTML = "";
-
-  root.appendChild(renderItems([foundPersonaje]));
-});
-
-
-
-export const example = () => {
-  return "example";
+  return filteredData;
 };
 
-export const sortBy = (data,sortBy,sortOrder) => {
-  if(sortOrder === "asc") {
-    return data.sort((a,b) => (a[sortBy] > b[sortBy]) ? 1:-1)
+export const sortBy = (data, sortBy, sortOrder) => {
+  if (sortOrder === "asc") {
+    return data.sort((a, b) => (a[sortBy] > b[sortBy] ? 1 : -1));
   } else if (sortOrder === "desc") {
-    return data.sort((a,b) => (a[sortBy] < b[sortBy]) ? 1:-1)
+    return data.sort((a, b) => (a[sortBy] < b[sortBy] ? 1 : -1));
   }
-  throw new Error("")
+  throw new Error("");
 };
--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
-import { example } from './dataFunctions.js';
-import { renderItems } from './view.js';
+import { filterData } from "./dataFunctions.js";
+import { renderItems } from "./view.js";
 
-import data from './data/dataset.js';
+import data from "./data/dataset.js";
 
 const root = document.getElementById("root");
+const nombresSelect = document.getElementById("nombres-select");
 
 root.appendChild(renderItems(data));
 
-console.log(example, renderItems(data), data);
+nombresSelect.addEventListener("input", (event) => {
+  const selectedName = event.target.value;
+  const personajes = data;
 
+  const filteredPersonaje = filterData(personajes, "name", selectedName);
+
+  root.innerHTML = "";
+
+  root.appendChild(renderItems(filteredPersonaje));
+});
