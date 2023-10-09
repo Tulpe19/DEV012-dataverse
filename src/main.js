2da4c9ccba38a51718d41c49be70169fadd004db
Juliana Guzmán <juliguzmac1906@gmail.com>
2023-10-09 10:25:41 -0500

    Se corrigió la funcion de filtrar y ordenar, se agrego el hecho género y se puede filtrar por genero. Tambien se puede ordenar por los otros hechos asc y desc

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import { filterData } from "./dataFunctions.js";
+import { sortBy } from "./dataFunctions.js";
 import { renderItems } from "./view.js";
 
 import data from "./data/dataset.js";
 
 const root = document.getElementById("root");
-const nombresSelect = document.getElementById("nombres-select");
-
+const nombresSelect = document.querySelector('select[name="personajes"]')
+const generoSelect = document.querySelector('select[name="genero"]')
+const hechoSelect = document.querySelector('select[name="hechos"]')
+const ordenSelect = document.querySelector('select[name="ordenAlfabetico"]')
 root.appendChild(renderItems(data));
 
-nombresSelect.addEventListener("input", (event) => {
+nombresSelect.addEventListener("change", (event) => {
   const selectedName = event.target.value;
   const personajes = data;
 
@@ -18,3 +21,37 @@ nombresSelect.addEventListener("input", (event) => {
 
   root.appendChild(renderItems(filteredPersonaje));
 });
+
+generoSelect.addEventListener("change", (event) => {
+  const selectedGender = event.target.value;
+  const personajes = data;
+
+  const filteredPersonajes = filterData(personajes, "Género", selectedGender);
+
+  root.innerHTML = "";
+
+  root.appendChild(renderItems(filteredPersonajes));
+});
+
+function ordenar() {
+  const selectedHecho = hechoSelect.value
+  const selectedOrden = ordenSelect.value
+  //console.log(selectedHecho, selectedOrden)
+
+  const personajes = data;
+
+  const orderedPersonajes = sortBy(personajes, selectedHecho, selectedOrden);
+
+  root.innerHTML = "";
+
+  root.appendChild(renderItems(orderedPersonajes));
+
+}
+
+ordenSelect.addEventListener("change", (event) => {
+  ordenar()
+})
+
+hechoSelect.addEventListener("change", (event) => {
+  ordenar()
+})
