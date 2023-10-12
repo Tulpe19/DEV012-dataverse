0ba343cfe3bb77ee7ce96ec99c6f4289a8a46b12
Milton Di Simone <miltondisimone@gmail.com>
2023-10-12 17:06:50 -0300

    Se cambió el ciclo for of por for each

--- src/view.js
+++ src/view.js
@@ -2,33 +2,30 @@ export const renderItems = (data) => {
   // Aquí comienza tu código y puedes retornar lo que tu necesites
 
   const ul = document.createElement("ul");
-  
-  for(const item of data) {
-    const li = document.createElement("li")
 
-    const img = document.createElement("img")
+  data.forEach((item) => {
+    const li = document.createElement("li");
 
-    img.src = item.imageUrl
+    const img = document.createElement("img");
 
-    li.innerHTML+= `<dd itemprop="name">${item.name}</dd>`
-    li.appendChild(img)
+    img.src = item.imageUrl;
+
+    li.innerHTML += `<dd itemprop="name">${item.name}</dd>`;
+    li.appendChild(img);
 
     li.innerHTML += `
     <dl itemscope itemtype="">
       
       <dt>Descripción corta:</dt><dd itemprop="shortDescription">${item.shortDescription}</dd>
       <dt>Hechos:</dt><dd itemprop="facts">
-      <dt>Comida favorita:</dt><dd itemprop="comidaFavorita">${item.facts['Comida favorita']}</dd>
-      <dt>Ocupación:</dt><dd itemprop="ocupacion">${item.facts['Ocupación']}</dd>
-      <dt>Frase célebre:</dt><dd itemprop="frase">${item.facts['Frase celebre']}</dd>
+      <dt>Comida favorita:</dt><dd itemprop="comidaFavorita">${item.facts["Comida favorita"]}</dd>
+      <dt>Ocupación:</dt><dd itemprop="ocupacion">${item.facts["Ocupación"]}</dd>
+      <dt>Frase célebre:</dt><dd itemprop="frase">${item.facts["Frase celebre"]}</dd>
     </dl>
-    `
+    `;
 
-    ul.appendChild(li)
+    ul.appendChild(li);
+  });
 
-  }
-
-  return ul
-  
+  return ul;
 };
-
